4f07beeaab470a94d090d8223902b7ca6cd592a6
pj7810 <pj7810@sohu.com>
2023-06-11 01:25:35 +0800

    add handle /healthz

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", printHeader)
+	http.HandleFunc("/healthz", healthz)
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
@@ -15,7 +17,18 @@ func main() {
 }
 
 func printHeader(w http.ResponseWriter, r *http.Request) {
-	for k, v := range r.Header {
-		fmt.Println(k, v)
+	for k, vs := range r.Header {
+		for _, v := range vs {
+			w.Header().Add(k, v)
+		}
 	}
+	w.Header().Add("VERSION", os.Getenv("VERSION"))
+	if r.Header.Get("X-FORWARDED-FOR") != "" {
+		fmt.Println("forwarded:", r.Header.Get("X-FORWARDED-FOR"))
+	}
+	fmt.Println("RemoteAddr", r.RemoteAddr)
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
 }
